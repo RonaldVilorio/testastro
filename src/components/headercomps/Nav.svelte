ede5074f55d33660b92ec6dc33d7c848ba38ac44
RonaldVilorio <ronvilorio@gmail.com>
2022-08-12 11:09:15 -0400

    added Svelte component and imported into astro file

---
+++ src/components/headercomps/Nav.svelte
@@ -0,0 +1,15 @@
+<script>
+  const navItems = ['Pages', 'Resources', 'Blog'];
+</script>
+
+<nav
+  class="items-center w-full md:w-auto hidden md:flex text-gray-600 dark:text-slate-200"
+  aria-label="Main navigation"
+  id="menu"
+>
+  <ul>
+    <li>Hey</li>
+    <li>hey</li>
+    <li>hey</li>
+  </ul>
+</nav>
