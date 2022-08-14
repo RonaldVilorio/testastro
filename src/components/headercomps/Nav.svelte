12ef773567fc0ab1025aadd965367fe89416bebd
RonaldVilorio <ronvilorio@gmail.com>
2022-08-14 10:31:47 -0400

    clip path polygon for nav

--- src/components/headercomps/Nav.svelte
+++ src/components/headercomps/Nav.svelte
@@ -2,7 +2,7 @@
   const navItems = ['Projects', 'About'];
 </script>
 
-<nav class="mx-auto mt-6 p-2 max-w-fit bg-white">
+<nav class="mx-auto mt-6 p-2 max-w-fit bg-green-400 ribbon">
   <ul class="flex">
     {#each navItems as item}
       <li class="mx-5">{item}</li>
@@ -11,4 +11,7 @@
 </nav>
 
 <style>
+  .ribbon {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 50% 70%, 0 100%);
+  }
 </style>
